c63194c29a4fa469ee2987a9fd868a215241af78
ADx96 <aboodrous@gmail.com>
2021-02-02 00:12:02 +0300

    Solved the Conditionals task4

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
 // Define the numbers and the operation here
-
+let number = 5;
+let number2 = 7;
+let operation = "*";
 // Write if-statements here
+if (operation === "*" )
+    console.log(" the operation has " + operation +" 5 * 7 = " + (number*number2));
+if (operation === "-")
+console.log(" the operation has " + operation +" 5 - 7 = " + (number-number2));
+if (operation === "+")
+console.log(" the operation has " + operation +" 5 = 7 = " + (number+number2));
+if (operation === "/")
+console.log(" the operation has " + operation +" 5 / 7 = " + (number/number2));
